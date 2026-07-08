6f03f1f4e8b70d9a6683780fb3c647aa37680d70
budget-pixel <peydevon@gmail.com>
2026-07-08 13:36:22 -0500

    patched mobile issue search bar

--- assets/nav.js
+++ assets/nav.js
@@ -85,7 +85,7 @@
       mobileStylesheet.id = mobileStylesheetId;
       mobileStylesheet.rel = "stylesheet";
     }
-    mobileStylesheet.href = wcBudgetAssetBaseUrl + "mobile.css?v=23";
+    mobileStylesheet.href = wcBudgetAssetBaseUrl + "mobile.css?v=24";
     document.head.appendChild(mobileStylesheet);
   }
   function loadWcScriptOnce(scriptId, src, onload){
